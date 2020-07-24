164b5f67aa43d0e1b8f509562f3e3666890767e9
mehrdad9119 <47624515+mehrdad9119@users.noreply.github.com>
2020-07-24 11:15:16 -0700

    Add rank for users (#6)
    Co-authored-by: Mehrdad Rahanjam <mrahanjam@apple.com>

--- packages/front-end/src/components/Rank/Rank.js
+++ packages/front-end/src/components/Rank/Rank.js
@@ -1,16 +1,47 @@
 import React from 'react';
 
-const Rank = ({ name, entries }) => {
-  return (
-    <div>
-      <div className='white f3'>
-        {`${name}, your current entry count is...`}
+class Rank extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      rank: ''
+    }
+  }
+
+  componentDidMount() {
+    this.generateRank(this.props.entries)
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.entries === this.props.entries && prevProps.name === this.props.name) {
+      return;
+    }
+    this.generateRank(this.props.entries);
+  }
+
+  generateRank = (entries) => {
+    fetch(`https://1qshfcatw8.execute-api.us-east-1.amazonaws.com/prod/rank?rank=${entries}`)
+      .then(res => res.json())
+      .then(data => this.setState({ rank: data.input}))
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    const { name, entries } = this.props 
+    return (
+      <div>
+        <div className='white f3'>
+          {`${name}, your current entry count is...`}
+        </div>
+        <div className='white f1'>
+          {entries}
+        </div>
+        <div className='white f3'>
+          {`Rank: ${this.state.rank}`}
+        </div>
       </div>
-      <div className='white f1'>
-        {entries}
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Rank;
